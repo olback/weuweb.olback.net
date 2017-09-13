d175e6f42e4497ae9192077cbbb5835eec1416a8
olback <git@olback.net>
2017-09-13 21:12:21 +0200

    Updated profile.php

--- page/profile.php
+++ page/profile.php
@@ -1,11 +1,22 @@
 <?php require "login/loginheader.php"; ?>
+
 <article class="content">
-    <h1>Profile:</h1>
-    Username: <?php echo $_SESSION['username'];?><br>
-    Name: <?php echo $_SESSION['name'];?><br>
-    Class: <?php echo $_SESSION['class'];?><br>
-    Email: <?php echo $_SESSION['email'];?><br>
-    Admin: <?php echo $_SESSION['admin'];?>
+<h1>Profile</h1>
+    <div class="title-search-block">
+        <div class="title-block">
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="title-description"> View your profile information. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <strong>Username:</strong> <?php echo $_SESSION['username'];?><br>
+    <strong>Name:</strong> <?php echo $_SESSION['name'];?><br>
+    <strong>Class:</strong> <?php echo $_SESSION['class'];?><br>
+    <strong>Email:</strong> <?php echo $_SESSION['email'];?><br>
+    <strong>Admin:</strong> <?php if($_SESSION['admin'] == "1"){echo "Yes"} else {echo "No"}?>
     <br><br>
     <p>Later on when I've got time, there will be more features here. You'll be able to change your email and password.</p>
 </article>
