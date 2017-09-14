580e84e3abae187e6555ab2800e4b2c24255ebd2
Edwin <git@olback.net>
2017-09-14 15:33:35 +0200

    Let's go home

--- page/main.php
+++ page/main.php
@@ -3,7 +3,7 @@
 <article class="content">
     <div class="title-block">
         <h2 class="title"> <?php echo $site_name; ?> </h2>
-        <p class="title-description"> Uploaded code examples by users. </p>
+        <p class="title-description"> I guess you could say it's some kind of a forum? </p>
     </div>
     <div class="col-md-6">
         <p>Currently under construction.<br></p>
