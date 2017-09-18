ec0631de46bf1e94a76907df730419d3082cca2b
Edwin <git@olback.net>
2017-09-18 15:24:40 +0200

    Cursor styling

--- page/profile.php
+++ page/profile.php
@@ -23,28 +23,28 @@
                             <label class="control-label">Username</label>
                             <div class="input-group">
                                 <input id="username" readonly="readonly" class="form-control" placeholder="Username" type="text" value="<?php echo $_SESSION['username'];?>">
-                                <span class="input-group-addon" onclick="enableInput('username')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
+                                <span class="input-group-addon click" onclick="enableInput('username')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label">Name</label>
                             <div class="input-group">
-                                <input readonly="readonly" class="form-control" placeholder="Name" type="text" value="<?php echo $_SESSION['name'];?>">
-                                <span class="input-group-addon"><i class="fa fa-pencil" aria-hidden="true"></i></span>
+                                <input id="name" readonly="readonly" class="form-control" placeholder="Name" type="text" value="<?php echo $_SESSION['name'];?>">
+                                <span class="input-group-addon click" onclick="enableInput('name')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label">Class</label>
                             <div class="input-group">
-                                <input readonly="readonly" class="form-control" placeholder="Class" type="text" value="<?php echo $_SESSION['class'];?>">
-                                <span class="input-group-addon"><i class="fa fa-pencil" aria-hidden="true"></i></span>
+                                <input id="class" readonly="readonly" class="form-control" placeholder="Class" type="text" value="<?php echo $_SESSION['class'];?>">
+                                <span class="input-group-addon click" onclick="enableInput('class')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label">Email</label>
                             <div class="input-group">
-                                <input readonly="readonly" class="form-control" placeholder="Email" type="email" value="<?php echo $_SESSION['email'];?>">
-                                <span class="input-group-addon"><i class="fa fa-pencil" aria-hidden="true"></i></span>
+                                <input id="email" readonly="readonly" class="form-control" placeholder="Email" type="email" value="<?php echo $_SESSION['email'];?>">
+                                <span class="input-group-addon click" onclick="enableInput('email')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                             </div>
                         </div>
                         <button type="button" id="updateProfile" class="btn btn-primary btn-lg" disabled="disabled">Submit</button>
