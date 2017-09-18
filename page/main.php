707e1a7f97b1813c6e6acd57c28825996dd62eb5
olback <git@olback.net>
2017-09-18 19:19:32 +0200

    Fixed layout issue

--- page/main.php
+++ page/main.php
@@ -6,25 +6,24 @@
         <p class="title-description"> I guess you could say it's some kind of a forum? </p>
     </div>
     <div class="col-md-6">
-        <!--<p>Currently under construction.<br></p>-->
         <section class="section">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="card card-default">
-                                    <div class="card-header">
-                                        <div class="header-block">
-                                            <p class="title"> Update! </p>
-                                        </div>
-                                    </div>
-                                    <div class="card-block">
-                                        <p>You're now able to change your profile information. This includes username, name, class and your email. You're going to be able to update your password as well in the near future.</p>
-                                    </div>
-                                    <div class="card-footer"> 19:13 18-09-2017</div>
-                                </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-default">
+                        <div class="card-header">
+                            <div class="header-block">
+                                <p class="title"> Update! </p>
                             </div>
-                            <!-- /.col-xl-4 -->
                         </div>
-                        <!-- /.row -->
-                    </section>
+                        <div class="card-block">
+                            <p>You're now able to change your profile information. This includes username, name, class and your email. You're going to be able to update your password as well in the near future.</p>
+                        </div>
+                        <div class="card-footer"> 19:13 18-09-2017</div>
+                    </div>
+                </div>
+                <!-- /.col-md-12 -->
+            </div>
+            <!-- /.row -->
+        </section>
     </div>
 </article>
