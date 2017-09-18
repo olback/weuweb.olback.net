e413677d7865036b54692b53bf6a52be0e45c641
olback <git@olback.net>
2017-09-18 17:48:14 +0200

    test

--- page/profile.php
+++ page/profile.php
@@ -1,53 +1,53 @@
 <?php require "login/loginheader.php"; ?>
 
+<?php
+
+if(isset($_POST['updateProfile'])) {
+    echo '<script>alert("yeeeeeeet");</script>'
+}
+
+
+?>
+
 <article class="content">
     <div class="title-block">
         <h2 class="title"> Profile </h2>
         <p class="title-description"> View/update your profile information. </p>
     </div>
-    <!--<div class="col-md-6">
-        <strong>Username:</strong> <?php echo $_SESSION['username'];?><br>
-        <strong>Name:</strong> <?php echo $_SESSION['name'];?><br>
-        <strong>Class:</strong> <?php echo $_SESSION['class'];?><br>
-        <strong>Email:</strong> <?php echo $_SESSION['email'];?><br>
-        <strong>Admin:</strong> <?php if($_SESSION['admin'] == "1"){echo "Yes";} else {echo "No";}?>
-        <br><br>
-        <p>Later on when I've got time, there will be more features here. You'll be able to change your email and password.</p>
-    </div>-->
     <section class="section">
         <div class="row sameheight-container">
             <div class="col-md-6">
                 <div class="card card-block">
-                    <form>
+                    <form method="POST">
                         <div class="form-group">
                             <label class="control-label">Username</label>
                             <div class="input-group">
-                                <input id="username" readonly="readonly" class="form-control" placeholder="Username" type="text" value="<?php echo $_SESSION['username'];?>">
+                                <input id="username" name="username" readonly="readonly" class="form-control" placeholder="Username" type="text" value="<?php echo $_SESSION['username'];?>">
                                 <span class="input-group-addon click" onclick="enableInput('username')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label">Name</label>
                             <div class="input-group">
-                                <input id="name" readonly="readonly" class="form-control" placeholder="Name" type="text" value="<?php echo $_SESSION['name'];?>">
+                                <input id="name" name="name" readonly="readonly" class="form-control" placeholder="Name" type="text" value="<?php echo $_SESSION['name'];?>">
                                 <span class="input-group-addon click" onclick="enableInput('name')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label">Class</label>
                             <div class="input-group">
-                                <input id="class" readonly="readonly" class="form-control" placeholder="Class" type="text" value="<?php echo $_SESSION['class'];?>">
+                                <input id="class" name="class" readonly="readonly" class="form-control" placeholder="Class" type="text" value="<?php echo $_SESSION['class'];?>">
                                 <span class="input-group-addon click" onclick="enableInput('class')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label">Email</label>
                             <div class="input-group">
-                                <input id="email" readonly="readonly" class="form-control" placeholder="Email" type="email" value="<?php echo $_SESSION['email'];?>">
+                                <input id="email" name="email" readonly="readonly" class="form-control" placeholder="Email" type="email" value="<?php echo $_SESSION['email'];?>">
                                 <span class="input-group-addon click" onclick="enableInput('email')"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                             </div>
                         </div>
-                        <button type="button" id="updateProfile" class="btn btn-primary btn-lg" disabled="disabled">Submit</button>
+                        <button type="button" id="updateProfile" name="updateProfile" class="btn btn-primary btn-lg" disabled="disabled">Submit</button>
                     </form>
                 </div>
             </div> <!-- .../col-md-6 -->
@@ -55,16 +55,20 @@
             <!-- Not implemented yet -->
             <div class="col-md-6">
                 <div class="card card-block">
-                    <form>
+                    <form method="POST">
+                    <div class="form-group">
+                            <label class="control-label">Current password</label>
+                            <input class="form-control boxed" type="password" name="cPassword">
+                        </div>
                         <div class="form-group">
                             <label class="control-label">New password</label>
-                            <input class="form-control boxed" type="password" disabled="disabled">
+                            <input class="form-control boxed" type="password" name="nPassword1">
                         </div>
                         <div class="form-group">
                             <label class="control-label">Repeat password</label>
-                            <input class="form-control boxed" type="password" disabled="disabled">
+                            <input class="form-control boxed" type="password" name="nPassword2">
                         </div>
-                        <button type="button" id="updatePassword" class="btn btn-primary btn-lg" disabled="disabled">Submit</button>
+                        <button type="button" id="updatePassword" name="updatePassword" class="btn btn-primary btn-lg" disabled="disabled">Submit</button>
                     </form>
                 </div>
             </div> <!-- .../col-md-6 -->
