965fa717513bcbbb9536b994fe0b7bbe01a53a1f
Edwin <git@olback.net>
2017-09-18 15:19:12 +0200

    Some JS

--- js/profile.js
+++ js/profile.js
@@ -2,7 +2,11 @@
  *  JavaScript for the prfile page on weuweb.olback.net
  */
 
+const profileInfoButton = document.getElementById('updateProfile');
+const updatePasswordButton = document.getElementById('updatePassword');
+
 function enableInput(id) {
     var input = document.getElementById(id);
-    input.readonly = false;
+    input.readOnly = false;
+    profileInfoButton.disabled = false;
 }
